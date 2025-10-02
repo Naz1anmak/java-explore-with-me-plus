2a5f1a460cd92c671016a3235ad4dda906ee33c5
Mult1k33 <Dmitry.kan@yandex.ru>
2025-10-02 17:21:54 +0300

    Add implementation stats-client module

--- stats/stats-client/src/main/java/ru/practicum/StatsClient.java
+++ stats/stats-client/src/main/java/ru/practicum/StatsClient.java
@@ -1,19 +1,61 @@
 package ru.practicum;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@Slf4j
 @Component
 public class StatsClient {
-    final RestClient restClient;
-    final String statUrl;
+    private final RestClient restClient;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-    public StatsClient(@Value("${client.url}") String statUrl) {
-        this.restClient = RestClient.create();
-        this.statUrl = statUrl;
+    public StatsClient(@Value("${stats-server.url:http://localhost:9090}") String serverUrl) {
+        this.restClient = RestClient.builder()
+                .baseUrl(serverUrl)
+                .defaultHeader("Content-Type", "application/json")
+                .build();
     }
 
-    public static void main(String[] args) {
+    public void saveHit(CreateEndpointHitDto createEndpointHitDto) {
+        restClient.post()
+                .uri("/hit")
+                .body(createEndpointHitDto)
+                .retrieve()
+                .toBodilessEntity();
+
+        log.debug("Запись обращения к эндпоинту успешно сохранена: приложение {}, URI {}",
+                createEndpointHitDto.app(), createEndpointHitDto.uri());
+    }
+
+    public List<ViewStatsDto> getStats(LocalDateTime start, LocalDateTime end,
+                                       List<String> uris, Boolean unique) {
+        String startStr = start.format(formatter);
+        String endStr = end.format(formatter);
+
+        ViewStatsDto[] stats = restClient.get()
+                .uri(uriBuilder -> {
+                    uriBuilder.path("/stats")
+                            .queryParam("start", startStr)
+                            .queryParam("end", endStr);
+                    if (uris != null && !uris.isEmpty()) {
+                        uriBuilder.queryParam("uris", String.join(",", uris));
+                    }
+                    if (unique != null) {
+                        uriBuilder.queryParam("unique", unique);
+                    }
+                    return uriBuilder.build();
+                })
+                .retrieve()
+                .body(ViewStatsDto[].class);
+
+        List<ViewStatsDto> result = stats != null ? List.of(stats) : List.of();
+        log.debug("Получено записей статистики  {}", result.size());
+        return result;
     }
 }
