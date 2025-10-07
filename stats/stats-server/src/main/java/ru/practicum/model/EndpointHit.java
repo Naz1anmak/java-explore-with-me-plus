168bbfb6700adeed59b26cb8a72b100edc8dcef9
Alexander <fign9polna@gmail.com>
2025-10-08 02:49:38 +0300

    Update application properties for environment variables and enhance local configurations

--- stats/stats-server/src/main/java/ru/practicum/model/EndpointHit.java
+++ stats/stats-server/src/main/java/ru/practicum/model/EndpointHit.java
@@ -1,19 +1,21 @@
 package ru.practicum.model;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "endpoint_hits")
 @Getter
-@Setter(AccessLevel.PROTECTED)
-@ToString
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class EndpointHit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     @Column(name = "hit_id")
     private Long id;
 
@@ -21,23 +23,4 @@ public class EndpointHit {
     private String uri;
     private String ip;
     private LocalDateTime timestamp;
-
-    public EndpointHit(String app, String uri, String ip, LocalDateTime timestamp) {
-        this.app = app;
-        this.uri = uri;
-        this.ip = ip;
-        this.timestamp = timestamp;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof EndpointHit)) return false;
-        return id != null && id.equals(((EndpointHit) o).getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return id == null ? 0 : id.hashCode();
-    }
 }
